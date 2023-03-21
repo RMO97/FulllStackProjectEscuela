fd72b8226af7471a76afeb6373d5e296fad80a35
RMO97 <belmont2097@gmail.com>
2023-03-21 10:01:45 -0600

    Project API

---
+++ Escuela API/Escuela API/Models/Estudiante.cs
@@ -0,0 +1,11 @@
+﻿namespace Escuela_API.Models
+{
+    public class Estudiante
+    {
+        public  Guid Id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad { get; set; }
+        public string genero { get; set; }
+    }
+}
---
+++ Escuela API/Escuela API/Models/Maestro.cs
@@ -0,0 +1,11 @@
+﻿namespace Escuela_API.Models
+{
+    public class Maestro
+    {
+        public Guid Id { get; set; }
+
+        public string nombreMtro { get; set; }
+
+        public int edadMtro { get; set; }
+    }
+}
